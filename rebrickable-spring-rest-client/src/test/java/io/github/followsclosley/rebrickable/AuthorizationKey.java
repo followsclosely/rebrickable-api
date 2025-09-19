a78a1eacfc92435420b93172952ac17d4dfce9b6
Matthew L. Wilson <mlavwilson@gmail.com>
2025-09-19 07:53:16 -0400

    Initial Entry

--- rebrickable-spring-rest-client/src/test/java/io/github/followsclosley/rebrickable/AuthorizationKey.java
+++ rebrickable-spring-rest-client/src/test/java/io/github/followsclosley/rebrickable/AuthorizationKey.java
@@ -2,25 +2,32 @@ package io.github.followsclosley.rebrickable;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 /**
  * Utility class to load the authorization key from application.properties file.
  */
+
 public class AuthorizationKey {
     public static String VALUE;
 
     static {
         Properties props = new Properties();
-        try (InputStream input = AuthorizationKey.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (input != null) {
-                props.load(input);
-                VALUE = props.getProperty("key");
-            } else {
-                throw new RuntimeException("application.properties not found");
+        for (String resource : List.of("application.properties", ".application.properties")) {
+            try (InputStream input = AuthorizationKey.class.getClassLoader().getResourceAsStream(resource)) {
+                if (input != null) {
+                    props.load(input);
+                    VALUE = props.getProperty("key");
+                    System.out.println("Authorization key loaded from \"" + resource + "\"");
+                }
+            } catch (IOException e) {
+                System.out.println("ERROR: loading key from " + resource + ": " + e.getMessage());
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load key from application.properties", e);
+        }
+
+        if( VALUE == null || VALUE.isEmpty() || "<<insert your key here>>".equals(VALUE)) {
+            throw new IllegalStateException("Your authorization key not found in application.properties or .application.properties");
         }
     }
 }
