e49d8a62058239aeddcc0239fa20ec831924a02c
Matthew L. Wilson <mlavwilson@gmail.com>
2025-09-19 07:35:54 -0400

    Initial Entry

--- rebrickable-api/src/main/java/io/github/followsclosley/rebrickable/catalog/RebrkColorCatalog.java
+++ rebrickable-api/src/main/java/io/github/followsclosley/rebrickable/catalog/RebrkColorCatalog.java
@@ -5,6 +5,16 @@ import io.github.followsclosley.rebrickable.dto.RebrkColor;
 import java.io.IOException;
 import java.util.stream.Stream;
 
+/**
+ * Provides access to a catalog of RebrkColor objects.
+ * Implementations of this interface allow streaming all available colors from the catalog.
+ */
 public interface RebrkColorCatalog {
+    /**
+     * Returns a stream of {@link RebrkColor} objects from the catalog.
+     *
+     * @return a {@code Stream} of {@code RebrkColor} objects
+     * @throws IOException if an I/O error occurs while accessing the catalog
+     */
     Stream<RebrkColor> streamFromCatalog() throws IOException;
 }
